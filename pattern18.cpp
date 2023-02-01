03cf112e0441e9546e2159e35c542c776ef6b0b4
Yashasvi R P <yashasvirp@gmail.com>
2023-02-01 10:31:14 +0530

    added another method in pattern file

--- pattern18.cpp
+++ pattern18.cpp
@@ -28,6 +28,21 @@ int main(){
     }
   }
 
+
+  //Another way
+  
+  for(int i = 0; i < n; i++){
+
+    cout<<endl;
+
+    for(int j = 0; j <= i; j++){
+      c = 'A' + n - 1 - i + j; 
+      cout<<c;
+    }
+  }
+  
+      
+
   cout<<endl;
   return 0;
 }
