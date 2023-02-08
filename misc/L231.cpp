1e1b39f8e17516af7433644f3a67183b91248f6e
Yashasvi R P <yashasvirp@gmail.com>
2023-02-08 10:20:00 +0530

    Added L231.cpp

---
+++ misc/L231.cpp
@@ -0,0 +1,24 @@
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2^x.
+*/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int ans = 1, i = 0;
+
+        while(i < 31){
+
+            if(n == ans)
+                return true;
+            if(ans < INT_MAX/2)
+                ans*=2;
+            
+            i++;
+        }
+
+        return false;
+    }
+};
