03e0a64b5207122b07436c69d69dd2079dcde435
Yashasvi R P <yashasvirp@gmail.com>
2023-02-09 09:01:43 +0530

    Added a better way to solve the question using bit manipulation

--- misc/L231.cpp
+++ misc/L231.cpp
@@ -7,17 +7,21 @@ An integer n is a power of two, if there exists an integer x such that n == 2^x.
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int ans = 1, i = 0;
 
-        while(i < 31){
+      // int ans = 1, i = 0;
 
-            if(n == ans)
-                return true;
-            if(ans < INT_MAX/2)
-                ans*=2;
+        // while(i < 31){
+
+        //     if(n == ans)
+        //         return true;
+        //     if(ans < INT_MAX/2)
+        //         ans*=2;
             
-            i++;
-        }
+        //     i++;
+        // }
+
+      if(n&n-1 == 0)
+	return true;
 
         return false;
     }
