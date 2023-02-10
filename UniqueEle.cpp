eff3775689e6a6c5fda7ca8324cc6e8ec860efb1
Yashasvi R P <yashasvirp@gmail.com>
2023-02-10 09:26:30 +0530

    Finding ONE unique element

---
+++ UniqueEle.cpp
@@ -0,0 +1,37 @@
+// Ana array of size N is given where N = 2M+1. (basically N is odd)
+//M numbers are present twice, and only 1 number is present once.
+// Find the unique element.
+
+#include<iostream>
+using namespace std;
+
+
+int findUnique(int arr[],int n){
+  int p = arr[0];
+
+  for(int i = 1; i < n; i++) // XORing with 2 duplicate elements will cancel
+    p = p^arr[i];            // each other out
+
+  return p;
+  
+}
+
+int main(){
+
+  int n, arr[100], p;
+
+  cout<<"Enter size: ";
+  cin>>n;
+
+  cout<<"Enter elements: ";
+  
+  for(int i = 0; i < n; i++)
+    cin>>arr[i];
+
+  
+  cout<<findUnique(arr,n);  
+  
+  cout<<endl;
+  
+  return 0;
+}
