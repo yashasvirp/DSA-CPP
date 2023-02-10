a003e90495c72bb13fcd46c084ac152d7a64d858
Yashasvi R P <yashasvirp@gmail.com>
2023-02-10 09:01:17 +0530

    Swapping alternate elements

---
+++ SwapAlternate.cpp
@@ -0,0 +1,38 @@
+/* This program is to swap alternate elements in array */
+
+//If the number of elements in array are odd, then the last number remains same
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int n, arr[100];
+
+  cout<<"Enter size: ";
+  cin>>n;
+
+  cout<<"Enter elements: ";
+
+  int p = (n%2==0)?n:n-1;
+  
+  for(int i = 0; i < n; i++)
+    cin>>arr[i];
+  
+  for(int i = 0; i < p; i+=2){
+    int temp;
+    temp = arr[i];
+    arr[i] = arr[i+1];
+    arr[i+1] = temp;
+  }
+
+  cout<<"After swapping: ";
+
+  for(int i = 0; i < n; i++)
+    cout<<arr[i]<<" ";
+
+  cout<<endl;
+
+  return 0;
+
+}
